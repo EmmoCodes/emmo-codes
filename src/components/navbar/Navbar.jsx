ed146bbd496b2e2cd57177719a26f2a7381812c5
S4nt1nO <Gianluca1996@gmx.de>
2024-05-12 00:59:13 +0200

    project changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,15 +5,9 @@ import { motion } from 'framer-motion'
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
       <Sidebar />
       <div className="wrapper">
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}>
-          Emmo Codes
-        </motion.span>
+        <span></span>
         <div className="social">
           <a href="#">
             <img src="/public/mail.svg" alt="" />
